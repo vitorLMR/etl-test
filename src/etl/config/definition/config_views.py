89b1a0bc642290154c8428dfddf48794f0d21bca
Vitor <vitor.rosa@innovaconnect.com.br>
2025-04-01 22:12:24 -0300

    FIX: super

--- src/etl/config/definition/config_views.py
+++ src/etl/config/definition/config_views.py
@@ -3,7 +3,7 @@ from core.env.env import Env
 
 from config.definition.views.dataset_ai_view import DatasetAiView
 
+
 class ConfigViews(WorkView):
     def __init__(self, env: Env):
-        super.__init__([DatasetAiView(env)])
-        pass
+        super().__init__([DatasetAiView(env)])
